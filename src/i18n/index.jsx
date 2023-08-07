e467fd31ab09d3badf29ca55690802f5882b0aa3
milad-emami <milad.emami1025@gmail.com>
2023-08-07 16:40:37 +0330

    feat: add persian and other translations

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,37 +1,39 @@
-import arMessages from './messages/ar.json';
-import caMessages from './messages/ca.json';
+import arMessages from "./messages/ar.json";
+import faIRMessages from "./messages/fa_IR.json";
+import caMessages from "./messages/ca.json";
 // no need to import en messages-- they are in the defaultMessage field
-import dedeMessages from './messages/de_DE.json';
-import es419Messages from './messages/es_419.json';
-import frMessages from './messages/fr.json';
-import heMessages from './messages/he.json';
-import hiMessages from './messages/hi.json';
-import idMessages from './messages/id.json';
-import ititMessages from './messages/it_IT.json';
-import kokrMessages from './messages/ko_kr.json';
-import plMessages from './messages/pl.json';
-import ptbrMessages from './messages/pt_br.json';
-import ptptMessages from './messages/pt_PT.json';
-import ruMessages from './messages/ru.json';
-import thMessages from './messages/th.json';
-import ukMessages from './messages/uk.json';
-import zhcnMessages from './messages/zh_CN.json';
+import dedeMessages from "./messages/de_DE.json";
+import es419Messages from "./messages/es_419.json";
+import frMessages from "./messages/fr.json";
+import heMessages from "./messages/he.json";
+import hiMessages from "./messages/hi.json";
+import idMessages from "./messages/id.json";
+import ititMessages from "./messages/it_IT.json";
+import kokrMessages from "./messages/ko_kr.json";
+import plMessages from "./messages/pl.json";
+import ptbrMessages from "./messages/pt_br.json";
+import ptptMessages from "./messages/pt_PT.json";
+import ruMessages from "./messages/ru.json";
+import thMessages from "./messages/th.json";
+import ukMessages from "./messages/uk.json";
+import zhcnMessages from "./messages/zh_CN.json";
 
 const messages = {
   ar: arMessages,
+  fa: faIRMessages,
   es: es419Messages, // Prospectus uses es language code for spanish, added `es` option and pointed to es-419 strings.
-  'es-419': es419Messages,
+  "es-419": es419Messages,
   fr: frMessages,
-  'zh-cn': zhcnMessages,
-  'it-it': ititMessages,
-  'pt-pt': ptptMessages,
-  'de-de': dedeMessages,
+  "zh-cn": zhcnMessages,
+  "it-it": ititMessages,
+  "pt-pt": ptptMessages,
+  "de-de": dedeMessages,
   ca: caMessages,
   he: heMessages,
   id: idMessages,
-  'ko-kr': kokrMessages,
+  "ko-kr": kokrMessages,
   pl: plMessages,
-  'pt-br': ptbrMessages,
+  "pt-br": ptbrMessages,
   ru: ruMessages,
   th: thMessages,
   uk: ukMessages,
