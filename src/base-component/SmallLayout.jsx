fc60edc08ab33e4c4b1eda0c9fbc6d833cf4b061
Khaled <info@khaledr.ir>
2023-11-26 15:50:41 +0330

    fix: add logo in responsive

--- src/base-component/SmallLayout.jsx
+++ src/base-component/SmallLayout.jsx
@@ -5,17 +5,20 @@ import { useIntl } from '@edx/frontend-platform/i18n';
 import { Hyperlink, Image } from '@edx/paragon';
 import classNames from 'classnames';
 
+import DefaultLogo from '../assets/NavLogo-placeholder.svg';
+import useGetConfig from '../data/useGetConfig';
 import messages from './messages';
 
 const SmallLayout = () => {
   const { formatMessage } = useIntl();
+  const { headerLogo } = useGetConfig();
 
   return (
     <span className="bg-primary-400 w-100">
       <div className="col-md-12 small-screen-top-stripe" />
       <div>
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-          <Image className="logo-small" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
+          <Image className="logo-small" alt={getConfig().SITE_NAME} src={headerLogo || DefaultLogo} />
         </Hyperlink>
         <div className="d-flex align-items-center mb-3 mt-3 mr-3">
           <div className={classNames({ 'small-yellow-line mr-n2.5': getConfig().SITE_NAME === 'edX' })} />
