4740d0a5ad5e43d0d15c4b0902e0ecf684c2c536
milad-emami <milad.emami1025@gmail.com>
2024-12-18 12:45:58 +0330

    fix: update logo

--- src/common-components/useGetConfig.js
+++ src/common-components/useGetConfig.js
@@ -11,21 +11,25 @@ const useGetConfig = () => {
   };
   const { data, isLoading, isError } = useQuery(
     'headerLogo',
-    () => fetchConfig({
-      baseURL: getConfig().LMS_BASE_URL,
-      instanceConfigAPIUrl: getConfig().AC_INSTANCE_CONFIG_API_URL,
-    }),
+    () =>
+      fetchConfig({
+        baseURL: getConfig().LMS_BASE_URL,
+        instanceConfigAPIUrl: getConfig().AC_INSTANCE_CONFIG_API_URL,
+      }),
     {
       enabled:
         !!getConfig().LMS_BASE_URL && !!getConfig().AC_INSTANCE_CONFIG_API_URL,
-    },
+    }
   );
-  const faviconVersion = Date.now(); // Update this version number when the favicon updates
+  const currentVersion = Date.now(); // This creates a unique timestamp
+  const faviconVersion = currentVersion; // Update this version number when the favicon updates
   const favicon = data?.favicon
     ? `${data.favicon}?v=${faviconVersion}`
     : faviconPlaceholder;
+  const headerLogo = data?.logo ? `${data.logo}?v=${currentVersion}` : null;
+
   return {
-    headerLogo: data?.logo,
+    headerLogo,
     hasBilling: data?.has_billing,
     favicon,
     platformName: data?.platform_name,
